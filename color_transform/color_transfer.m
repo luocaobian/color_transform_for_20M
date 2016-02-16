ed14275e6d41324f08c71ba956ba48abd9cbdf55
Zerong Xi <xizerong@gmail.com>
2016-02-16 14:52:12 -0800

    re: get ct1sat pics for cam3

--- color_transform/color_transfer.m
+++ color_transform/color_transfer.m
@@ -2,16 +2,16 @@ function [ res ] = color_transfer( img_path, ys, sat_mat )
 %COLOR_TRANSFER Summary of this function goes here
 %   Detailed explanation goes here
 tic;
-spmd
 img=imread(img_path);
+res = zeros(size(img,1),size(img,2),size(img,3));
+temp = zeros(3,1);
 for row = 1:size(img,1)
     for col = 1:size(img,2)
          for rgb = 1:3
-            res(row,col,rgb) = ys(img(row,col,rgb)+1,rgb);
+            temp(rgb) = ys(img(row,col,rgb)+1,rgb);
          end
-    res(row,col,:)=sat_mat*squeeze(res(row,col,:));
+    res(row,col,:)=sat_mat*temp;
     end
-end
 end
 
 toc;
