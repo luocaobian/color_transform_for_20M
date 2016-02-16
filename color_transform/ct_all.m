261bc273b55feb44d3d9730a62b26644abf22d3d
Zerong Xi <xizerong@gmail.com>
2016-02-16 15:05:56 -0800

    get ctasat pics for cam3

--- color_transform/ct_all.m
+++ color_transform/ct_all.m
@@ -6,7 +6,7 @@ tic;
 fnames = dir('Robot*c03.jpg');
 numfids = length(fnames);
 for i = 1:numfids
-    imwrite(color_transfer(fnames(i).name, ys, sat_mat), strcat('ct1sat-',fnames(i).name));
+    imwrite(color_transfer(fnames(i).name, ys, sat_mat), strcat('ctasat-',fnames(i).name));
 end
 
 toc
