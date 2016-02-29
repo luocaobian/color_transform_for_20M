08bf2219af859826643b606083c00e74fcf5d1f9
zxi <xizerong@gmail.com>
2016-02-29 14:51:43 -0800

    change in&out format for color transform
    in & out are pointers to address

--- ColorTransformCPP/ColorTransformCPP/main.cpp
+++ ColorTransformCPP/ColorTransformCPP/main.cpp
@@ -11,11 +11,6 @@
 
 
 int main(int argc, const char * argv[]) {
-    colorTransform cam3("/users/zxi/Documents/MyCode/color_transform_for_20M/cam3_sat.txt", "/users/zxi/Documents/MyCode/color_transform_for_20M/cam3_curve.txt");
-    std::vector<int> in_pixel = {0, 24, 49};
-    std::vector<int> out_pixel = cam3.ct_pixel(in_pixel);
-    std::cout << "Almost finished" << std::endl;
-    std::cout << out_pixel[0] << std::endl;
     
     return 0;
 }
