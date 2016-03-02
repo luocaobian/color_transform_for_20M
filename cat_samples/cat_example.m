7e1e20ee2fe3894c5d111fd4b41dcb95ae030ae9
Zerong Xi <xizerong@gmail.com>
2016-03-01 19:51:58 -0800

    cat&resize all samples

--- cat_samples/cat_example.m
+++ cat_samples/cat_example.m
@@ -1,10 +1,37 @@
-cam = {'c00','c01','c02','c03'};
+in = '/media/zxi/My Passport/robotscenes-samples/';
+out = '/lab/zxi/Documents/cat_samples/';
+parfor s = 1:9
+    for p = 1:4
+        for c = 1:4
+            file = strcat('RobotScene-s000',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
+end
 
-% for c = 1:4
-%     cat_samples(cam{c});
-% end
+for s = 10:99
+    for p = 1:4
+        for c = 1:4
+            file = strcat('RobotScene-s00',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
+end
 
+for s = 100:999
+    for p = 1:4
+        for c = 1:4
+            file = strcat('RobotScene-s0',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
+end
 
-for c = 1:4
-    imwrite(from49to7by7(imread(strcat(cam{c},'.png'))),strcat(cam{c},'-7by7.png'));
+for s = 1000:1000
+    for p = 1:4
+        for c = 1:4
+            file = strcat('RobotScene-s',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
 end
--- cat_samples/cat_samples.m
+++ cat_samples/cat_samples.m
@@ -1,19 +1,19 @@
-function [  ] = cat_samples( cam )
+function [  ] = cat_samples( in_path, out_path )
 %CAT_SAMPLES Summary of this function goes here
 %   Detailed explanation goes here
 
 tic;
 
-fnames = dir(strcat('*',cam,'*.png'));
-numfids = length(fnames);
-big_pic = imread(fnames(1).name);
-for i = 2:numfids
-    big_pic = cat(1, big_pic, imread(fnames(i).name));
+if exist(strcat(in_path,'-000010.png'), 'file')
+big_pic = imresize(imread(strcat(in_path,'-000010.png')),0.5);
+for i = 1:10
+    big_pic = cat(1, big_pic, imresize(imread(strcat(in_path,'-00000',int2str(i-1),'.png')),0.5));
 end
 
-imwrite(big_pic, strcat(cam,'.png'));
+imwrite(big_pic, strcat(out_path,'.png'));
 
 toc
 
 end
 
+end
