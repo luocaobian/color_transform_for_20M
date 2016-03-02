3ad92e738938fa3ca974eb5383734cec895a34c3
Zerong Xi <xizerong@gmail.com>
2016-03-02 09:29:54 -0800

    cat & resize all samples (2)

--- cat_samples/cat_example.m
+++ cat_samples/cat_example.m
@@ -1,36 +1,45 @@
 in = '/media/zxi/My Passport/robotscenes-samples/';
 out = '/lab/zxi/Documents/cat_samples/';
-parfor s = 1:9
-    for p = 1:4
-        for c = 1:4
-            file = strcat('RobotScene-s000',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
-            cat_samples(strcat(in,file),strcat(out,file));
-        end
-    end
-end
+% parfor s = 1:9
+%     for p = 1:4
+%         for c = 1:4
+%             file = strcat('RobotScene-s000',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+%             cat_samples(strcat(in,file),strcat(out,file));
+%         end
+%     end
+% end
+% 
+% for s = 10:99
+%     for p = 1:4
+%         parfor c = 1:4
+%             file = strcat('RobotScene-s00',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+%             cat_samples(strcat(in,file),strcat(out,file));
+%         end
+%     end
+% end
+% 
+% for s = 100:999
+%     for p = 1:4
+%         parfor c = 1:4
+%             file = strcat('RobotScene-s0',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+%             cat_samples(strcat(in,file),strcat(out,file));
+%         end
+%     end
+% end
+% 
+% for s = 1000:1000
+%     parfor p = 1:4
+%         for c = 1:4
+%             file = strcat('RobotScene-s',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+%             cat_samples(strcat(in,file),strcat(out,file));
+%         end
+%     end
+% end
 
-for s = 10:99
-    for p = 1:4
+for s = 3000:3000
+    parfor p = 11:20
         for c = 1:4
-            file = strcat('RobotScene-s00',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
-            cat_samples(strcat(in,file),strcat(out,file));
-        end
-    end
-end
-
-for s = 100:999
-    for p = 1:4
-        for c = 1:4
-            file = strcat('RobotScene-s0',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
-            cat_samples(strcat(in,file),strcat(out,file));
-        end
-    end
-end
-
-for s = 1000:1000
-    for p = 1:4
-        for c = 1:4
-            file = strcat('RobotScene-s',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            file = strcat('RobotScene-s',int2str(s),'-p',int2str(p-1),'-c0',int2str(c-1));
             cat_samples(strcat(in,file),strcat(out,file));
         end
     end
