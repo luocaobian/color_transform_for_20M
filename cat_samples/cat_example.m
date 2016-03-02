99ef30dfb5e3e836c384e4fba2f4a69a8d89d59c
Zerong Xi <xizerong@gmail.com>
2016-03-02 14:28:55 -0800

    trivial change

--- cat_samples/cat_example.m
+++ cat_samples/cat_example.m
@@ -1,5 +1,5 @@
 in = '/media/zxi/My Passport/robotscenes-samples/';
-out = '/lab/zxi/Documents/cat_samples/';
+out = '/lab/zxi/Documents/added_samples/';
 % parfor s = 1:9
 %     for p = 1:4
 %         for c = 1:4
@@ -9,14 +9,14 @@ out = '/lab/zxi/Documents/cat_samples/';
 %     end
 % end
 % 
-% for s = 10:99
-%     for p = 1:4
-%         parfor c = 1:4
-%             file = strcat('RobotScene-s00',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
-%             cat_samples(strcat(in,file),strcat(out,file));
-%         end
-%     end
-% end
+for s = 19:19
+    for p = 1:4
+        parfor c = 1:4
+            file = strcat('RobotScene-s00',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
+end
 % 
 % for s = 100:999
 %     for p = 1:4
@@ -36,8 +36,17 @@ out = '/lab/zxi/Documents/cat_samples/';
 %     end
 % end
 
-for s = 3000:3000
-    parfor p = 11:20
+for s = 3000:3001
+    parfor p = 1:10
+        for c = 1:4
+            file = strcat('RobotScene-s',int2str(s),'-p0',int2str(p-1),'-c0',int2str(c-1));
+            cat_samples(strcat(in,file),strcat(out,file));
+        end
+    end
+end
+
+for s = 3000:3001
+    parfor p = 11:21
         for c = 1:4
             file = strcat('RobotScene-s',int2str(s),'-p',int2str(p-1),'-c0',int2str(c-1));
             cat_samples(strcat(in,file),strcat(out,file));
